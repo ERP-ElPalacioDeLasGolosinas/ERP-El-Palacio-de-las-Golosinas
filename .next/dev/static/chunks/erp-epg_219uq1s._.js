61314c92b772f236354d5836a833bfcb5ababecd
Samuel Arzelan Haro <samuelarzelan18@gmail.com>
2026-08-26 21:21:28 -0300

    .next/dev/static borrado

--- .next/dev/static/chunks/erp-epg_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/erp-epg_src_app_globals_1ewywqd.css","static/chunks/03q7_next_20z3u4x._.js","static/chunks/03q7_@supabase_auth-js_dist_module_1el-xkc._.js","static/chunks/03q7_14cfral._.js","static/chunks/erp-epg_src_0ca3bb2._.js","static/chunks/erp-epg_1d3xbd2._.js"],
-    source: "entry"
-});
